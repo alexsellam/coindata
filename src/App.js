c80c79bbb51a7da77d835d54fd724c3099830cf7
User <user@macbook-pro-de-user-3.home>
2022-03-04 17:16:20 +0100

    adding react router 6

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import logo from './logo.svg';
 import React, {useState, useEffect} from 'react'
 import axios from 'axios'
-
+import {Routes, Route} from 'react-router-dom'
 import Coins from './components/Coins'
+import Coin from './routes/Coin'
 import Navbar from './components/Navbar'
 
 function App() {
 
   const [coins, setCoins] = useState([]);
 
-  const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=10&page=1&sparkline=false'
+  const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=50&page=1&sparkline=false'
   useEffect(() => {
     axios.get(url).then((response) => {
       setCoins(response.data)
@@ -22,7 +23,13 @@ function App() {
   return (
     <>
     <Navbar />
-    <Coins coins={coins}/>
+    <Routes>
+      <Route path='/' element={<Coins coins={coins}/>} />
+      <Route path='/coin' element={<Coin />}>
+        <Route path=':coinId' element={<Coin />} />
+      </Route>
+    </Routes>
+    
     </>
   );
 }
