b2ab088824e47efafc6cd47c0b27ed18e37f861a
User <alex.sellam@gmail.com>
2022-03-06 23:54:33 +0100

    new title in navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,7 @@ const Navbar = () => {
     <Link to='/'>
         <div className="navbar">
             <FaCoins className='icon'/>
-            <h1>Coin <span className="purple">Data</span></h1>
+            <h1>Coin <span className="purple">Data in €</span></h1>
         </div>
     </Link>
   )
